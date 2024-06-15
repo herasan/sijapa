6d7eff3290f52113d1fccaf231e29be0ad0d692c
herasan <kiritonazuma@gmail.com>
2024-06-15 08:36:24 +0700

    header admin

--- application/views/admin/layout/v_header.php
+++ application/views/admin/layout/v_header.php
@@ -19,10 +19,16 @@
 
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <?= $this->session->userdata('nm_pengguna'); ?> <b></b>
+                        <i class="fa fa-user fa-fw"></i> <?= $this->session->userdata('nm_pengguna'); ?> <b class="caret"></b>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
 
                     </ul>
                 </li>
